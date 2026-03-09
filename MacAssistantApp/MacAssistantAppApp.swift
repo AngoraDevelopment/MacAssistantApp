04283f375a6a404b90dcab1f7ce12e21afec858f
Edgardo Ramos <smokeshadowedgardo123@gmail.com>
2026-03-09 00:32:10 -0700

    Initial Commit

---
+++ MacAssistantApp/MacAssistantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAssistantAppApp.swift
+//  MacAssistantApp
+//
+//  Created by Edgardo Ramos on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct MacAssistantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
